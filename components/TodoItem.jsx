a80bb6b66e7ca3b9879cc43d36dc44dd11b85131
Oliver Otchere <Oliverotchere4@gmail.com>
2022-01-04 05:51:03 +0000

    added delet icon

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-const TodoItem = ({item, removeTodo}) => {
-    return (
-        <TouchableOpacity 
-            onPress={()=>removeTodo(item.key)}>
-          <Text style={styles.item}>
-              {item.text}
-          </Text>
-        </TouchableOpacity>
-    )
-}
+const TodoItem = ({ item, removeTodo }) => {
+  return (
+    <TouchableOpacity onPress={() => removeTodo(item.key)}>
+      <View style={styles.item}>
+        <MaterialIcons name={"delete"} size={18} color={"#333"} />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
 
 const styles = StyleSheet.create({
-    item:{
-        padding: 16,
-        marginTop: 16,
-        borderColor: "#bbb",
-        borderWidth: 1,
-        borderStyle: "dashed",
-        borderRadius: 10,
-
-    }
-})
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+    flexDirection:"row"
+  },
+  itemText:{
+      marginLeft: 10
+  }
+});
